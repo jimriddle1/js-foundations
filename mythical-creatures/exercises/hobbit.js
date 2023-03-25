3679a1671b45d4d3ed79549e7feb3491b3bcea1c
Jim Riddle <jimriddle0@gmail.com>
2023-03-25 15:43:50 -0500

    finished hobbit

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,12 +1,54 @@
-function createHobbit(name, age = 0){
-  const dragon = {name: input_name, rider: input_rider, temperment: input_temperment, timesEaten: 0, hungry: true}
- 
+function createHobbit(name = 'unknown', age = 0){
+  const hobbit = {name: name, age: age, isAdult: false, isOld: false, acquaintances: []}
+  return hobbit
+}
+
+function celebrateBirthday(hobbit){
+  let older_hobbit = hobbit
+  older_hobbit.age += 1
+  if (older_hobbit.age >= 33){
+    older_hobbit.isAdult = true
+  }
+  if (older_hobbit.age >= 101){
+    older_hobbit.isOld = true
+  }
+  return older_hobbit
+}
+
+function getRing(hobbit){
+  if (hobbit.name == 'Frodo'){
+    return 'Here is the ring!'
+  }else{
+    return 'You can\'t have it!'
+  }
+}
+
+function meetPeople(hobbit, people_array){
+  let socialHobbit = hobbit
+  let array = hobbit.acquaintances
+  for (people of people_array){
+    array.push(people)
+  }
+  socialHobbit.acquaintances = array 
+  return socialHobbit
+}
+
+function findFriends(hobbit){
+  array = [];
+  people = hobbit.acquaintances
+  for (person of people){
+    if(person.relationship == 'friend'){
+      array.push(person.name)
+    }
+  }
+  return array
+
 }
 
 module.exports = {
   createHobbit, 
-  // celebrateBirthday, 
-  // getRing, 
-  // meetPeople, 
-  // findFriends
+  celebrateBirthday, 
+  getRing, 
+  meetPeople, 
+  findFriends
 }

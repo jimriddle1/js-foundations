637d9faeeb77f9eba9a46a8cb2253f92fbd069f6
Jim Riddle <jimriddle0@gmail.com>
2023-03-25 13:41:49 -0500

    finished dragons

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,34 @@
+function createDragon(input_name, input_rider, input_temperment){
+  const dragon = {name: input_name, rider: input_rider, temperment: input_temperment, timesEaten: 0, hungry: true}
+  return dragon
+}
 
+function greetRider(dragon){
+  return "Hi, " + dragon.rider + "!"
+}
+
+function eat(input_dragon){
+  dragon = input_dragon
+  dragon.timesEaten += 1
+  if (dragon.timesEaten >= 3){
+    dragon.hungry = false
+  }
+  return dragon
+}
+
+function findFireBreathers(dragons){
+  array = [];
+  for (dragon of dragons){
+    if(dragon.temperment == 'aggressive'){
+      array.push(dragon)
+    }
+  }
+  return array;
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,7 +1,10 @@
-
+function createHobbit(name, age = 0){
+  const dragon = {name: input_name, rider: input_rider, temperment: input_temperment, timesEaten: 0, hungry: true}
+ 
+}
 
 module.exports = {
-  // createHobbit, 
+  createHobbit, 
   // celebrateBirthday, 
   // getRing, 
   // meetPeople, 

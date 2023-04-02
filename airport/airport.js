0f4f03cdcb7a29e6474533ae83b468b8a952ec43
Jim Riddle <jimriddle0@gmail.com>
2023-04-02 11:17:04 -0500

    started working through airport

--- airport/airport.js
+++ airport/airport.js
@@ -1,7 +1,12 @@
 
 module.exports = { 
-  // createAirport, 
+  createAirport, 
   // welcomeGuests, 
   // landPlane, 
   // checkAirlineLocations
 };
+
+function createAirport(name, airlines = [], availableGates = 0){
+  const airport = {name: name, airlines: airlines, availableGates: availableGates}
+  return airport;
+}

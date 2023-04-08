b3f14faa9003ee93494702f8eef777f321b7e389
Jim Riddle <jimriddle0@gmail.com>
2023-04-08 12:32:58 -0500

    finished barber shop

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,37 @@
 
 
-module.exports = {  };
+module.exports = {  
+    createBarber,
+    giveCompliment,
+    cutHair,
+    listStyles
+};
+
+
+
+function createBarber(name, earnings = 0, haircuts = []){
+    const barber = {name: name, earnings: earnings, haircuts: haircuts};
+    return barber;
+}
+
+function giveCompliment(haircut){
+    return "This " + haircut.style + " looks great!";
+}
+
+function cutHair(barber, haircut){
+    newBarber = barber
+    newBarber.earnings += haircut.price;
+    newBarber.haircuts.push(haircut);
+
+    return newBarber;
+}
+
+function listStyles(barber, length){
+    placeholder = [];
+    for (haircut of barber.haircuts){
+        if (length == haircut.hairLength){
+            placeholder.push(haircut.style);
+        }
+    }
+    return placeholder;
+}
